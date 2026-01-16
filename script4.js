013828c3fe3df143206bca2becfaf070a600343f
Shrey Shah <sshrey962@gmail.com>
2026-01-16 19:19:15 +0530

    Updated box model and display layout

---
+++ script4.js
@@ -0,0 +1,26 @@
+console.log("JavaScript is working");
+
+let name = "Shrey";
+const age = 24;
+
+let isStudent = true;
+let marks = 82.5;
+let city = "Bhavnagar";
+let result = null;
+let value;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(marks);
+console.log(city);
+console.log(result);
+console.log(value);
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof marks);
+console.log(typeof city);
+console.log(typeof result);
+console.log(typeof value);
